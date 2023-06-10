41051d0e3fad5b20362d3f54e3c258ace4e2e0be
Bohdan Rudenko <brudenko2804@gmail.com>
2023-06-10 21:05:46 +0300

    Define class Product

--- main_components/Product.h
+++ main_components/Product.h
@@ -2,8 +2,31 @@
 #define PROJECT_WAREHOUSE_PRODUCT_H
 
 
-class Product {
+#include <string>
 
+class Product {
+private:
+    std::string productID;
+    std::string name;
+    double price;
+    int quantity;
+
+public:
+    Product(const std::string &productId, const std::string &name, double price, int quantity)
+    : productID(productId), name(name), price(price), quantity(quantity)
+    {}
+
+    const std::string &getProductId() const { return productID; }
+    void setProductId(const std::string &productId) { productID = productId; }
+
+    const std::string &getName() const { return name; }
+    void setName(const std::string &name) { Product::name = name; }
+
+    double getPrice() const { return price; }
+    void setPrice(double price) { Product::price = price; }
+
+    int getQuantity() const { return quantity; }
+    void setQuantity(int quantity) { Product::quantity = quantity; }
 };
 
 
