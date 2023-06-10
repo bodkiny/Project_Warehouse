c542ae6bcfd0b9014e78e8b029a178bc9ab4633a
Bohdan Rudenko <brudenko2804@gmail.com>
2023-06-11 01:16:07 +0300

    Implement the IOrderRepository and IProductRepository interfaces

--- impl/InMemoryOrderRepository.h
+++ impl/InMemoryOrderRepository.h
@@ -1,10 +1,33 @@
 #ifndef PROJECT_WAREHOUSE_INMEMORYORDERREPOSITORY_H
 #define PROJECT_WAREHOUSE_INMEMORYORDERREPOSITORY_H
 
+#include <map>
 #include "../interfaces/IOrderRepository.h"
 
 class InMemoryOrderRepository : public IOrderRepository{
+private:
+    std::map<std::string, Order> orders;
 public:
+    void addOrder(const Order &order) override {
+        orders[order.getOrderId()] = order;
+    }
+
+    void removeOrder(const std::string &orderId) override {
+        orders.erase(orderId);
+    }
+
+    std::optional<Order> getOrder(const std::string &orderId) override {
+        auto it = orders.find(orderId);
+        if (it != orders.end()) {
+            return it->second;
+        }
+        return std::nullopt;
+    }
+
+    std::map<std::string, Order>& getAllOrders() override {
+        return orders;
+    }
+
     ~InMemoryOrderRepository() override = default;
 };
 
--- impl/InMemoryProductRepository.h
+++ impl/InMemoryProductRepository.h
@@ -1,10 +1,34 @@
 #ifndef PROJECT_WAREHOUSE_INMEMORYPRODUCTREPOSITORY_H
 #define PROJECT_WAREHOUSE_INMEMORYPRODUCTREPOSITORY_H
 
+#include <map>
 #include "../interfaces/IProductRepository.h"
 
 class InMemoryProductRepository : public IProductRepository {
+private:
+    std::map<std::string, Product> products;
+
 public:
+    void addProduct(const Product &product) override {
+        products[product.getProductId()] = product;
+    }
+
+    void removeProduct(const std::string &productId) override {
+        products.erase(productId);
+    }
+
+    std::optional<Product> getProduct(const std::string &productId) override {
+        auto it = products.find(productId);
+        if (it != products.end()) {
+            return it->second;
+        }
+        return std::nullopt;
+    }
+
+    std::map<std::string, Product>& getAllProducts() override {
+        return products;
+    }
+
     ~InMemoryProductRepository() override = default;
 };
 
