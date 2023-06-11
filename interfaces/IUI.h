bba186f4553ea35d0c3e1ea3d9d7a1bcaa85fcea
Bohdan Rudenko <brudenko2804@gmail.com>
2023-06-11 23:26:18 +0300

    Modify the IUI interface method parameters
    Due to changing the container for storing objects of the Product and Order classes in the Warehouse class from std::vector to std::map container

--- interfaces/IUI.h
+++ interfaces/IUI.h
@@ -1,16 +1,18 @@
 #ifndef PROJECT_WAREHOUSE_IUI_H
 #define PROJECT_WAREHOUSE_IUI_H
 
-#include <vector>
+#include <map>
 #include <string>
+#include <optional>
 #include "../main_components/Product.h"
 #include "../main_components/Order.h"
 
 class IUI {
 public:
     virtual void displayMenu() = 0;
-    virtual void displayProducts(const std::vector<Product>& products) = 0;
-    virtual void displayOrders(const std::vector<Order>& orders) = 0;
+    virtual void displayProducts(std::map<std::string, Product>& products) = 0;
+    virtual void displayOrders(const std::map<std::string, Order>& orders) = 0;
+    virtual void displayExactOrder(const std::optional<Order>& orderOpt) = 0;
     virtual void displayMessage(const std::string& message) = 0;
     virtual std::string getInput() = 0;
 
