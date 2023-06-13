7f431c3abe8aee682cfbeb6c9bb5fb99cb8d568e
Bohdan Rudenko <brudenko2804@gmail.com>
2023-06-14 01:01:54 +0300

    Implement a run process of the program

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
-#include <iostream>
+#include "main_components/Warehouse.h"
+#include "impl/InMemoryProductRepository.h"
+#include "impl/InMemoryOrderRepository.h"
+#include "impl/CommandLineUI.h"
+#include "impl/ConsoleProductGenerator.h"
+#include "impl/ConsoleOrderGenerator.h"
+#include "impl/BinaryFileSerializer.h"
+#include "impl/BinaryFileDeserializer.h"
 
 int main() {
+
+    InMemoryProductRepository productRepository;
+    InMemoryOrderRepository orderRepository;
+    CommandLineUI ui;
+    ConsoleProductGenerator consoleProductGenerator;
+    ConsoleOrderGenerator consoleOrderGenerator;
+    BinaryFileSerializer fileSerializer;
+    BinaryFileDeserializer fileDeserializer;
+
+
+    Warehouse warehouse(
+            productRepository,
+            orderRepository,
+            ui,
+            consoleProductGenerator,
+            consoleOrderGenerator,
+            fileSerializer);
+
+    fileDeserializer.deserialize(warehouse);
+    warehouse.processUserInput();
+    warehouse.serialize(warehouse);
 
     return 0;
 }
