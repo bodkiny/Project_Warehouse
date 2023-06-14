43c292c7cf6c40e42492aae2758f7415222bf575
Bohdan Rudenko <brudenko2804@gmail.com>
2023-06-14 23:00:31 +0300

    Add new methods to the ISalesReportGenerator interface

--- interfaces/ISalesReportGenerator.h
+++ interfaces/ISalesReportGenerator.h
@@ -1,12 +1,14 @@
 #ifndef PROJECT_WAREHOUSE_ISALESREPORTGENERATOR_H
 #define PROJECT_WAREHOUSE_ISALESREPORTGENERATOR_H
 
-#include <vector>
+#include <map>
 #include "../main_components/Order.h"
 
 class ISalesReportGenerator {
 public:
-    virtual void generateReport(const std::vector<Order>& orders) = 0;
+    virtual void generateReport(const std::map<std::string, Order>& orders) = 0;
+    virtual std::vector<std::string> getProcessedOrderIds() = 0;
+    virtual void addProcessedOrderIds(const std::vector<std::string>& processedOrderIds) = 0;
 
     virtual ~ISalesReportGenerator() = default;
 };
